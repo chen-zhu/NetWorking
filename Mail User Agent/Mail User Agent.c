d2873ee40a5f7297981963d51276f85e01be9a70
Stone (Chen) Zhu <chen_zhu@umail.ucsb.edu>
2015-12-30 19:38:14 -0600

    connect && say hello
    finished connecting to server and getting response from server

--- Mail User Agent/Mail User Agent.c
+++ Mail User Agent/Mail User Agent.c
@@ -25,4 +25,62 @@
 #define SMTP_PORT 25
 #define MAXLINE 1024 		
 
-char *Server_IP = "128.111.53.4";
+char *Server_IP = "128.111.53.4";//ucsb engineering 
+
+void main (int argc, char ** argv)
+{
+	char sendline[MAXLINE], recvline[MAXLINE];
+	char recipient_address[MAXLINE]; // "RCPT TO" address.
+	char sender_address[MAXLINE];	 // "MAIL FROM" adress.
+	char *temp;
+	
+	if (argc != 2)
+	{
+		printf("usage: ./EmailSender <recipient address,\"RCPT TO\">\n");
+		exit(0);
+	}
+	strcpy(recipient_address, argv[1]);
+
+	/* Establish a TCP connection with the main server */
+	int sockfd;
+	struct sockaddr_in serveraddr;
+
+	
+	sockfd = socket(AF_INET,SOCK_STREAM,0); 
+	
+	bzero(&serveraddr,sizeof(serveraddr)); 
+	serveraddr.sin_family = AF_INET; 
+	serveraddr.sin_addr.s_addr = inet_addr(Server_IP); 
+	serveraddr.sin_port=htons(SMTP_PORT); 
+	
+	connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	
+	/* Read greeting from the server */
+	read(sockfd, recvline, MAXLINE);
+	printf("%s\n", recvline);	
+	temp = strtok(recvline, " ");
+	if (strcmp(temp, "220")!=0)
+	{
+		printf("220 reply ont received from server.\n");
+		exit(0);
+	}
+	
+	/* Send HELO command and get server response */
+	strcpy(sendline, "HELO alice\r\n");
+	printf("%s\n", sendline);
+	write(sockfd, sendline, strlen(sendline));
+
+	read(sockfd, recvline, MAXLINE);
+	printf("%s\n", recvline);
+	temp = strtok(recvline, " ");
+	if (strcmp(temp, "250")!=0)
+	{
+		printf("250 reply not received from server.\n");
+		exit(0);
+	}
+
+
+	//shut down the correspnding socket. 
+	shutdown(sockfd, 2); 
+	exit(0);	
+}  
